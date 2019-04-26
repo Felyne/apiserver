61cc544f76d07b1cd57ad4105f9206d41a100c81
Felyne <chenwd_135@qq.com>
2019-04-26 10:46:33 +0800

    update

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -21,6 +21,10 @@ type Context struct {
 	Username string
 }
 
+func getJwtSecret() string {
+	viper.GetString("jwt_secret")
+}
+
 // secretFunc validates the secret format.
 func secretFunc(secret string) jwt.Keyfunc {
 	return func(token *jwt.Token) (interface{}, error) {
@@ -63,7 +67,7 @@ func ParseRequest(c *gin.Context) (*Context, error) {
 	header := c.Request.Header.Get("Authorization")
 
 	// Load the jwt secret from config
-	secret := viper.GetString("jwt_secret")
+	secret := getJwtSecret()
 
 	if len(header) == 0 {
 		return &Context{}, ErrMissingHeader
@@ -79,7 +83,7 @@ func ParseRequest(c *gin.Context) (*Context, error) {
 func Sign(ctx *gin.Context, c Context, secret string) (tokenString string, err error) {
 	// Load the jwt secret from the Gin config if the secret isn't specified.
 	if secret == "" {
-		secret = viper.GetString("jwt_secret")
+		secret = getJwtSecret()
 	}
 	// The token content.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
